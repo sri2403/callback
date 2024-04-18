bc51da5d0151a73de7d3a8a5bc5facbcb4efef6e
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-04-18 21:22:43 +0530

    updated

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function displayCountdown(count, next) {
     var countdownElement = document.getElementById('countdown');
-    
+    var text=document.getElementById("text");
     setTimeout(function () {
+      text.innerHTML="wait";
       countdownElement.innerHTML = count;
       setTimeout(function () {
         countdownElement.innerHTML = '';
